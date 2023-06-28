a9594abcd7239231ee50eac912f49e3a73f65620
gritchin-artur <123599698+gritchin-artur@users.noreply.github.com>
2023-06-28 14:47:13 +0200

    1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,32 +1,32 @@
 
-export const Profile = () => {
-    return (
-        <div class="profile">
-            <div class="description">
-                <img
-                    src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-                    alt="User avatar"
-                    class="avatar"
-                />
-                <p class="name">fv</p>
-                <p class="tag">@pmarica</p>
-                <p class="location">Salvador, Brasil</p>
-            </div>
+export const Profile = ({username, avatar, tag, location, stats,}) => {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img
+   src={avatar}
+          alt="User avatar"
+          className="avatar"
+        />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-            <ul class="stats">
-                <li>
-                    <span class="label">Followers</span>
-                    <span class="quantity">1000</span>
-                </li>
-                <li>
-                    <span class="label">Views</span>
-                    <span class="quantity">2000</span>
-                </li>
-                <li>
-                    <span class="label">Likes</span>
-                    <span class="quantity">3000</span>
-                </li>
-            </ul>
-        </div>
-    );
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -1,46 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { App } from 'components/App';
 import './index.css';
 import user from './user.json';
-// import Profile from './components/Profile';
-
-
-
-const Profile =
-  <div class="profile">
-    <div class="description">
-      <img
-        src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-        alt="User avatar"
-        class="avatar"
-      />
-      <p class="name">{user.username}</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
-    </div>
-
-    <ul class="stats">
-      <li>
-        <span class="label">Followers</span>
-        <span class="quantity">1000</span>
-      </li>
-      <li>
-        <span class="label">Views</span>
-        <span class="quantity">2000</span>
-      </li>
-      <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
-      </li>
-    </ul>
-  </div>;
+import {Profile} from 'components/Profile';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
- {Profile}
-
-    {/* <App /> */}
+ <Profile
+  username={user.username}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
   </React.StrictMode>
 );
 
